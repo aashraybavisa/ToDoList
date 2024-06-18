3c23cead015d73ec8bd02943d5cbd189d2266d05
aashray-fan <a_bavisa@fanshaweonline.ca>
2024-06-18 14:13:06 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Aashray Bavisa on 2024-06-18.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
